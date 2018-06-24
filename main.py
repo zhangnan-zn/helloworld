751a24ab1e606170a08fdd84cfcdfd373686db18
Zhang Nan <zhannan@amazon.com>
2018-06-24 16:34:22 -0400

    Add second function

--- main.py
+++ main.py
@@ -7,5 +7,50 @@ def hello():
     return "hello world"
 
 
+@app.route("/second")
+def second():
+    return "second"
+
+
+@app.route("/third")
+def third():
+    return "third"
+
+
+@app.route("/fourth")
+def fourth():
+    return "fourth"
+
+
+@app.route("/fifth")
+def fifth():
+    return "fifth"
+
+
+@app.route("/sixth")
+def sixth():
+    return "sixth"
+
+
+@app.route("/seventh")
+def seventh():
+    return "seventh"
+
+
+@app.route("/eighth")
+def eighth():
+    return "eighth"
+
+
+@app.route("/ninth")
+def ninth():
+    return "ninth"
+
+
+@app.route("/tenth")
+def tenth():
+    return "tenth"
+
+
 if __name__ == "__main__":
     app.run()
